dfdd56b019a5a57535c6906edafcd3cdf60d88ad
iD Student <iD Student@CAMP-51178T2>
2019-07-11 10:00:17 -0400

    HIGH SCORE SCOREBOARD NEW SYSTEM AND NEW OBSTACLES

--- MYFINALFINALPROJECT/MYFINALFINALPROJECT.cpp
+++ MYFINALFINALPROJECT/MYFINALFINALPROJECT.cpp
@@ -11,6 +11,7 @@ sf::Vector2f velocity(0, 0);
 const int WIDTH = 1000, HEIGHT = 800;
 
 int Scoreboard = 0;
+int HighScore = 0;
 
 
 sf::Texture playerTex;
@@ -20,6 +21,8 @@ sf::Text myText;
 sf::Texture backgroundTex;
 sf::Texture aboveTex;
 sf::Texture belowTex;
+sf::Texture lupTex;
+sf::Texture ldownTex;
 
 
 void jump()
@@ -37,6 +40,8 @@ int main()
 	backgroundTex.loadFromFile("Textures/space.png");
 	aboveTex.loadFromFile("Textures/up2.png");
 	belowTex.loadFromFile("Textures/down2.png");
+	lupTex.loadFromFile("Textures/above3.png");
+	ldownTex.loadFromFile("Textures/below3.png");
 
 	bool movingRight = false;
 
@@ -45,23 +50,35 @@ int main()
 	myFont.loadFromFile("Fonts/Candal.ttf");
 
 
+	sf::Sprite ldownSprite;
+	ldownSprite.setTexture(ldownTex);
+	ldownSprite.setScale(0.8, 0.8);
+	ldownSprite.setOrigin(0, 350);
+	ldownSprite.setPosition(950, HEIGHT);
+
+
+	sf::Sprite lupSprite;
+	lupSprite.setTexture(lupTex);
+	lupSprite.setPosition(850, 0);
+	lupSprite.setScale(0.8, 0.8);
+
 	sf::Sprite belowSprite;
 	belowSprite.setTexture(belowTex);
 	belowSprite.setOrigin(0, 337);
-	belowSprite.setScale(0.7, 0.7);
+	belowSprite.setScale(0.8, 0.8);
 	belowSprite.setPosition(400, HEIGHT);
 
 
 	sf::Sprite aboveSprite;
 	aboveSprite.setTexture(aboveTex);
 	aboveSprite.setPosition(400, 0);
-	aboveSprite.setScale(0.6, 0.6);
+	aboveSprite.setScale(0.7, 0.7);
 
 
 	myText.setFont(myFont);
 	myText.setCharacterSize(40);
 	myText.setFillColor(sf::Color::Yellow);
-	myText.setPosition(700, 0);
+	myText.setPosition(650, 0);
 
 	sf::Sprite backgroundSprite;
 	backgroundSprite.setTexture(backgroundTex);
@@ -99,17 +116,19 @@ int main()
 
 		}
 
-		myText.setString(std::to_string(Scoreboard));
+		myText.setString("Score: " + std::to_string(Scoreboard) +"\nHigh Score: " + std::to_string(HighScore));
 
 
 		if (
 			playerSprite.getGlobalBounds().intersects(upSprite.getGlobalBounds()) ||
 			playerSprite.getGlobalBounds().intersects(downSprite.getGlobalBounds()) ||
 			playerSprite.getGlobalBounds().intersects(aboveSprite.getGlobalBounds()) ||
+			playerSprite.getGlobalBounds().intersects(lupSprite.getGlobalBounds()) ||
+			playerSprite.getGlobalBounds().intersects(ldownSprite.getGlobalBounds()) ||
 			playerSprite.getGlobalBounds().intersects(belowSprite.getGlobalBounds())
 			) {
 			playerSprite.setPosition(100, HEIGHT / 2);
-
+			Scoreboard = 0;
 
 		}
 
@@ -130,14 +149,24 @@ int main()
 		downSprite.move(-1, 0);
 		aboveSprite.move(-1, 0);
 		belowSprite.move(-1, 0);
+		lupSprite.move(-1, 0);
+		ldownSprite.move(-1, 0);
 
 		upSprite.getPosition().x;
 		downSprite.getPosition().x;
 		aboveSprite.getPosition().x;
 		belowSprite.getPosition().x;
+		lupSprite.getPosition().x;
+		ldownSprite.getPosition().x;
+
+		if (ldownSprite.getPosition().x < -500)
+			ldownSprite.move(WIDTH + 600, 0);
 
 		if (belowSprite.getPosition().x < -500)
 			belowSprite.move(WIDTH + 600, 0);
+
+		if (lupSprite.getPosition().x < -500)
+			lupSprite.move(WIDTH + 600, 0);
 
 		if (upSprite.getPosition().x < -500)
 			upSprite.move(WIDTH + 600, 0);
@@ -147,6 +176,8 @@ int main()
 
 		if (downSprite.getPosition().x < -500) {
 			Scoreboard++;
+			if (Scoreboard > HighScore)
+				HighScore = Scoreboard;
 			downSprite.move(WIDTH + 600, 0);
 		}
 
@@ -154,6 +185,8 @@ int main()
 
 		window.clear();
 		window.draw(backgroundSprite);
+		window.draw(ldownSprite);
+		window.draw(lupSprite);
 		window.draw(belowSprite);
 		window.draw(aboveSprite);
 		window.draw(upSprite);
